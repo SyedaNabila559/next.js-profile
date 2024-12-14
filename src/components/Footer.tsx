6974d740ec45da00fd202f7b235c9e8a52b90f6b
Syeda Nabila <161962167+SyedaNabila559@users.noreply.github.com>
2024-12-14 07:11:14 -0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-
 const Footer = () => {
   return (
-    <footer className="bg-black text-white text-center p-4 mt-0">
-      &copy; {new Date().getFullYear()} Nabila Bannay Khan. All rights reserved.
+    <footer className="bg-black text-white text-center p-4 md:p-6 lg:p-8 mt-4">
+      <p className="text-sm md:text-base lg:text-lg">
+        &copy; {new Date().getFullYear()} Nabila Bannay Khan. All rights reserved.
+      </p>
     </footer>
   );
 };
